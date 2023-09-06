6d65d323c1fb2aa0ec41ecc73a75df6fafc0d086
edasinar <56503188+edasinar@users.noreply.github.com>
2023-09-06 22:57:01 +0300

    Update and rename KDVHesaplama to Odev2.java

--- Java/Odev2.java
+++ Java/Odev2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class KDVHesaplama {
+public class Odev2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Para miktarı giriniz: ");
