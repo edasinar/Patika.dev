9725ea1d56d307775a3ca593e3556425eccd3ed7
edasinar <56503188+edasinar@users.noreply.github.com>
2023-09-06 22:32:18 +0300

    Create Main.java
    Java 101 dersi için verilen ilk ödev eklendi

---
+++ Java/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Matematik ders notunu giriniz: ");
+        int math = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Fizik ders notunu giriniz: ");
+        int physics = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Kimya ders notunu giriniz: ");
+        int chemistry = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Türkçe ders notunu giriniz: ");
+        int turkish = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Tarih ders notunu giriniz: ");
+        int history = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Müzik ders notunu giriniz: ");
+        int music = Integer.parseInt(sc.nextLine());
+
+        double avg = (math + physics + chemistry + turkish + history + music) / 6;
+
+        System.out.println(avg > 60 ? "Sınıfı geçti" : "Sınıfta kaldı");
+    }
+}
